4c0775a3201e77a8fc4f1c1a01387faa34076cea
AnthonyHuLu <jakitojhoans2304@hotmail.com>
2024-12-10 14:06:54 -0500

    mariadb version2

--- db/db/permisos.sql
+++ db/db/permisos.sql
@@ -20,3 +20,17 @@ CREATE TABLE mascotas (
     FOREIGN KEY (duenio_id) REFERENCES usuarios(id)
 );
 
+INSERT INTO usuarios (nombre, correo, hash_password, rol) VALUES
+('Admin', 'admin@empresa.com', 'superadmin', 'admin'),
+('Lucas', 'lucas@empresa.com', 'personallucas', 'personal'),
+('Tito', 'tito@empresa.com', 'personaltito', 'personal'),
+('John', 'john@gmail.com', 'jhonperro', 'duenio'),
+('Jane', 'jane@gmail.com', 'janegatoconejo', 'duenio'),
+('Roberto', 'roberto@gmail.com', 'robertosinmascota', 'duenio');
+
+INSERT INTO mascotas (nombre, tipo_mascota, duenio_id) VALUES
+('Fido', 'Perro', 4),
+('Cat', 'Gato', 5),
+('Bunny', 'Conejo', 5),
+('Matias', 'Perro', 2);
+
